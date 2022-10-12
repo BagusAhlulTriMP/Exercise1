7f59d280f9410b3c8e8611240cf7a2782a1d516d
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2022-10-12 17:49:11 +0700

    Add project files.

---
+++ Exercise1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Program
+    {
+        // Deklarasi array int ukuran 59
+        private int[] a = new int[59];
+
+        // Deklarasi variabel int untuk menyimpan banyaknya data pada array
+        private int n;
+
+        // Fungsi / Method untuk menerima masukan
+        public void read()
+        {
+            // Menerima angka untuk menentukan banyaknya data yang disimpan pada array
+            while (true)
+            {
+                Console.Write("Masukkan banyaknya elemen pada Array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 59)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal59 elemen\n");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine(" Masukkan elemen array");
+            Console.WriteLine("-------------------------");
+
+            // Pengguna memasukkan elemen pada array
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write("<" + (j + 1) + "> ");
+                string s1 = Console.ReadLine();
+                a[j] = Int32.Parse(s1);
+            }
+        }
+        public void display()
+        {
+            // Menampilkan array yang tersusun
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine(" Element array yang telah tersusun ");
+            Console.WriteLine("-----------------------------------");
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(a[j]);
+            }
+            Console.WriteLine("");
+        }
+
+        public void Exercise1Array()
+        {
+            for (int i = 1; i < n; i++) // For n - 1 passes
+            {
+                // Pada pass i, bandingkan n - i elemen pertama dengan elemen selanjutnya
+                for (int j = 0; j < n - i; j++)
+                {
+                    if (a[j] > a[j + 1]) // Jika elemen tidak dalam urutan yang benar
+                    {
+                        // Tukar elemen
+                        int temp;
+                        temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Creating the object of the Exercise1 class
+            Program myList = new Program();
+
+            // Pemanggilan fungsi untuk menerima elemen array
+            myList.read();
+            // Pemanggilan fungsi untuk mengurutkan array
+            myList.Exercise1Array();
+            // Pemanggilan fungsi untuk menampilkan array yang tersusun
+            myList.display();
+            // Exit
+            Console.WriteLine("\n\nTekan TombolApa saja untuk keluar.");
+            Console.Read();
+        }
+    }
+}
